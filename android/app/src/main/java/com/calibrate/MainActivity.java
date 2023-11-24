042835626d1eb7fa2a056fd73e0f138010dd5f15
Raushan Kumar <raushankumar6511@gmail.com>
2023-11-24 20:24:56 +0530

    Adding lottie splash screen

--- android/app/src/main/java/com/calibrate/MainActivity.java
+++ android/app/src/main/java/com/calibrate/MainActivity.java
@@ -4,6 +4,9 @@ import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
+import android.os.Bundle;
+import org.devio.rn.splashscreen.SplashScreen; // here
+
 
 public class MainActivity extends ReactActivity {
 
@@ -21,12 +24,21 @@ public class MainActivity extends ReactActivity {
    * DefaultReactActivityDelegate} which allows you to easily enable Fabric and Concurrent React
    * (aka React 18) with two boolean flags.
    */
+  // @Override
+  // protected ReactActivityDelegate createReactActivityDelegate() {
+  //   return new DefaultReactActivityDelegate(
+  //       this,
+  //       getMainComponentName(),
+  //       //  SplashScreen.show(this, R.id.lottie); // here
+  //       // SplashScreen.setAnimationFinished(true);
+  //       // If you opted-in for the New Architecture, we enable the Fabric Renderer.
+  //       DefaultNewArchitectureEntryPoint.getFabricEnabled());
+  // }
+
   @Override
-  protected ReactActivityDelegate createReactActivityDelegate() {
-    return new DefaultReactActivityDelegate(
-        this,
-        getMainComponentName(),
-        // If you opted-in for the New Architecture, we enable the Fabric Renderer.
-        DefaultNewArchitectureEntryPoint.getFabricEnabled());
+  protected void onCreate(Bundle savedInstanceState) {
+    SplashScreen.show(this, R.id.lottie); // here
+    SplashScreen.setAnimationFinished(true);
+    super.onCreate(null);
   }
 }
